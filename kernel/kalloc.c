c4d5b2dae489cf2154ea873bbb5db703e1329755
lghost <lghost1999@126.com>
2022-12-21 15:02:58 +0800

    finish Lab COW

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,10 +23,16 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct {
+  struct spinlock lock;
+  int cnt[PHYSTOP >> 12];
+} kref;
+
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+  initlock(&kref.lock, "kref");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +41,11 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    kref.cnt[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
+    
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,10 +60,19 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  acquire(&kref.lock);
+  kref.cnt[(uint64)pa >> 12]--;
+  if(kref.cnt[(uint64)pa >> 12] != 0) {
+    release(&kref.lock);
+    return;
+  }
+
+  release(&kref.lock);
 
   r = (struct run*)pa;
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
 
   acquire(&kmem.lock);
   r->next = kmem.freelist;
@@ -72,11 +90,27 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r) {
     kmem.freelist = r->next;
+    acquire(&kref.lock);
+    kref.cnt[(uint64)r >> 12] = 1;
+    release(&kref.lock);
+  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+}
+
+int getkref(uint64 pa) {
+  return kref.cnt[pa >> 12];
+}
+
+void addkref(uint64 pa) {
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    return;
+  acquire(&kref.lock);
+  kref.cnt[pa >> 12]++;
+  release(&kref.lock);
 }
